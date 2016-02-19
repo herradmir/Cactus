b228ad4ba0d2f459e4208045dbe9dde89d5fc371
Admir Husovic <mail.admir.husovic@gmail.com>
2016-02-19 16:04:25 +0100

    base login page

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -3,24 +3,36 @@
 angular.module('CactusApp.login', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
-    templateUrl: 'components/login/login.html',
-    controller: 'Login'
-  });
+	$routeProvider.when('/login', {
+		templateUrl: 'components/login/login.html',
+		controller: 'Login'
+	});
 }])
 
-.controller('Login', function($scope) {
-  $scope.userData = {
-    name:'',
-    password:''
-  };
-  $scope.firebaseUser= {
-    name:'Admir',
-    password:'password'
-  };
-// TODO get firebase service
-// TODO get user input
-  // TODO check user input with firebase data
-    // if true move to userpage with userid
-    // false show wrong password
+.controller('Login', function($scope, $location, $rootScope) {
+	$scope.userData = {
+		name: '',
+		password: ''
+	};
+	$scope.firebaseUser = {
+		name: 'Admir',
+		password: 'password'
+	};
+
+	$scope.loginForm = function() {
+			var name = $scope.userData.name;
+			var password = $scope.userData.password;
+			if ($scope.userData.name == $scope.firebaseUser.name && $scope.userData.password == $scope.firebaseUser.password) { // test
+				$rootScope.loggedUser = $scope.username;
+				$location.path("/overview");
+			} else {
+				$scope.loginError = "Invalid user/pass.";
+        console.log($scope.loginError);
+			}
+		}
+	// TODO get firebase service
+	// TODO get user input
+	// TODO check user input with firebase data
+	// if true move to userpage with userid
+	// false show wrong password
 });
