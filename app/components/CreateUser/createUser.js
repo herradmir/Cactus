fdd928f1ee628ebc2d82926be8bc9fe3efa7888d
Admir Husovic <mail.admir.husovic@gmail.com>
2016-02-25 14:02:06 +0100

    basic firebase user registration email & password

--- app/components/CreateUser/createUser.js
+++ app/components/CreateUser/createUser.js
@@ -3,12 +3,41 @@
 angular.module('CactusApp.createUser', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/register', {
-		templateUrl: 'components/CreateUser/createUser.html',
-		controller: 'createUser'
-	});
+  $routeProvider.when('/register', {
+    templateUrl: 'components/CreateUser/createUser.html',
+    controller: 'createUser'
+  });
 }])
 
-.controller('createUser', function($scope, $location, $rootScope) {
+.controller('createUser', function($scope, $location, $rootScope, $firebaseAuth, $firebaseObject, userRef) {
+
+  $scope.createUser = function createUser() {
+    var user = $scope.data;
+    var username = user.username;
+    if (user.password === user.reTypePassword) {
+      userRef.createUser({
+        email: user.email,
+        password: user.password
+      }, function(error, userData) {
+        if (error) {
+          console.log("Error creating user:", error);
+        } else {
+          console.log("Successfully created user account with uid:", userData.uid);
+        }
+      });
+      userRef.push({
+        email: user.email,
+        username: username,
+        tasks: [''],
+        group: ['']
+      });
+    }
+		else{
+			//trow error on password match
+			console.log('passwords dont match');
+		}
+
+  }
+
 
 });
