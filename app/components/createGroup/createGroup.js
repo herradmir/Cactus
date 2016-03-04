610d088b82642b53682c1a178f782b47fde8758a
Admir Husovic <mail.admir.husovic@gmail.com>
2016-03-04 11:25:05 +0100

    create group with fake fake default data

--- app/components/createGroup/createGroup.js
+++ app/components/createGroup/createGroup.js
@@ -2,13 +2,57 @@
 
 angular.module('CactusApp.createGroup', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/creategroup', {
-    templateUrl: 'components/createGroup/createGroup.html',
-    controller: 'createGroup'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/creategroup', {
+            templateUrl: 'components/createGroup/createGroup.html',
+            controller: 'createGroup'
+        });
+    }])
 
-.controller('createGroup', function($scope) {
+    .controller('createGroup', function ($scope, $firebaseAuth, $firebaseObject, $location, groupRef) {
 
-});
+        $scope.createGroup = function createGroup() {
+            var groupname = $scope.data.groupname;
+            var loggedInUser = 'John';
+            addGroupToFirebase(groupname, loggedInUser);
+            function addGroupToFirebase(groupname, loggedInUser) {
+                //  console.log('start added group name')
+                groupRef.push({
+                    groupID: 1234,
+                    name: groupname,
+                    groupmembers: [
+                        {
+                            userID: 1234,
+                            userName: loggedInUser
+                        }
+                    ],
+                    taskList: [{
+                        "id": 1,
+                        "taskName": "vacuum",
+                        "done": false
+                    }, {
+                            "id": 2,
+                            "taskName": "do the dishes",
+                            "done": false
+                        }, {
+                            "id": 3,
+                            "taskName": "clean the bathroom",
+                            "done": false
+                        }, {
+                            "id": 4,
+                            "taskName": "do the laundry",
+                            "done": false
+                        }, {
+                            "id": 5,
+                            "taskName": "take out the garbage",
+                            "done": false
+                        }
+                    ]
+                });
+                $location.path('/tasks');
+                //  console.log('end added group name')
+            }
+
+        }
+
+    });
