077089e15dd47ee565874084d8e3d1bad615953e
Admir Husovic <mail.admir.husovic@gmail.com>
2016-03-06 12:47:38 +0100

    add catch to tasks

--- app/components/tasks/tasks.js
+++ app/components/tasks/tasks.js
@@ -9,12 +9,15 @@ angular.module('CactusApp.tasks', ['ngRoute'])
         });
     }])
 
-    .controller('Tasks', function Tasks($scope, Auth, groupRef, groupList, $firebaseArray, $firebaseObject) {
+    .controller('Tasks', function Tasks($scope, Auth, groupList, $firebaseObject, $firebaseArray) {
         var loggedIn = Auth.$getAuth();
         var referenceToGroup = new Firebase('https://cactus-app.firebaseio.com/Groups/' + loggedIn.uid)
         var groupList = $firebaseObject(referenceToGroup);
 
         groupList.$loaded().then(function () {
             groupList.$bindTo($scope, "data")
-        });
+        })
+            .catch(function (error) {
+                console.log("Error:", error);
+            });
     });
