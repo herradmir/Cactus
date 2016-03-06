ef001deb3fd52af87ae1da8e2c76fcdd638a81b3
Admir Husovic <mail.admir.husovic@gmail.com>
2016-03-06 11:58:31 +0100

    three way binding tasks

--- app/components/tasks/tasks.js
+++ app/components/tasks/tasks.js
@@ -9,42 +9,12 @@ angular.module('CactusApp.tasks', ['ngRoute'])
         });
     }])
 
-    .controller('Tasks', function Tasks($scope, Auth, groupRef, groupList, $firebaseArray, $firebase) {
-
+    .controller('Tasks', function Tasks($scope, Auth, groupRef, groupList, $firebaseArray, $firebaseObject) {
         var loggedIn = Auth.$getAuth();
         var referenceToGroup = new Firebase('https://cactus-app.firebaseio.com/Groups/' + loggedIn.uid)
-        var taskList = $firebaseArray(referenceToGroup);
-        taskList.$loaded().then(function () {
-            angular.forEach(taskList, function (item) {
-                $scope.taskList = item.taskList;
-            });
+        var groupList = $firebaseObject(referenceToGroup);
+
+        groupList.$loaded().then(function () {
+            groupList.$bindTo($scope, "data")
         });
-
-        $scope.user = {
-            "userName": "Admir",
-            "tasks": []
-        };
-
-        // sync when checkbox changes true or false
-        $scope.sync = function (bool, task) {
-            for (var i = 0; i < $scope.taskList.length; i++) {
-                if ($scope.taskList[i].id == task.id) {
-                    $scope.taskList[i].done = bool;
-                }
-            }
-        }
-
-        $scope.maybeAdd = function (bool, task) {
-            if (bool) {
-                for (var j = 0; j < $scope.taskList.length; j++) {
-                    if ($scope.taskList[j].id == task.id) {
-                        $scope.taskList[j].doneBy.push({
-                            "user": $scope.user.userName,
-                            "date": new Date().getTime()
-                        });
-                        $scope.user.tasks.push(task);
-                    }
-                }
-            }
-        };
     });
