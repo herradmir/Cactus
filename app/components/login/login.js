cb35e1aef5a32a9f505991a4c714a5ed1897f871
Admir Husovic <mail.admir.husovic@gmail.com>
2016-03-12 14:21:51 +0100

    login

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -2,42 +2,43 @@
 
 angular.module('CactusApp.login', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
-    templateUrl: 'components/login/login.html',
-    controller: 'LoginController'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/login', {
+            templateUrl: 'components/login/login.html',
+            controller: 'LoginController'
+        });
+    }])
 
-.controller('LoginController', function($scope, $location, $rootScope, $firebaseAuth, $firebaseObject, Auth) {
+    .controller('LoginController', function($scope, $location, $rootScope, $firebaseAuth, $firebaseObject, Auth) {
+        $scope.location = $location
+        console.log($location.path());
+        $scope.logout = function logout() {
+            Auth.$unauth();
+            console.log('logging out');
+        };
 
-  $scope.logout = function logout() {
-    Auth.$unauth();
-    console.log('logging out');
-  };
+        $scope.loginForm = function login() {
+            var userName = $scope.userData.name;
+            var password = $scope.userData.password;
 
-  $scope.loginForm = function login() {
-      var userName = $scope.userData.name;
-      var password = $scope.userData.password;
+            Auth.$authWithPassword({
+                email: userName,
+                password: password
+            }, function(error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                } else {
+                    console.log("Authenticated successfully with payload:", authData);
+                }
+            });
 
-      Auth.$authWithPassword({
-        email: userName,
-        password: password
-      }, function(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          console.log("Authenticated successfully with payload:", authData);
-        }
-      });
-
-      Auth.$onAuth(function(authData) {
-        if (authData) {
-          console.log("Authenticated with uid:", authData.uid);
-          $location.path('/tasks');
-        } else {
-          console.log("Client unauthenticated.")
-        }
-      });
-    } // loginForm
-}); // module
+            Auth.$onAuth(function(authData) {
+                if (authData) {
+                    console.log("Authenticated with uid:", authData.uid);
+                    $location.path('/tasks');
+                } else {
+                    console.log("Client unauthenticated.")
+                }
+            });
+        } // loginForm
+    }); // module
